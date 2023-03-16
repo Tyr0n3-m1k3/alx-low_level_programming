bcee96278840a1d33dcb8799982512327b034bc1
Tyr0n3-m1k3 <truthuthi@gmail.com>
2023-03-16 14:28:32 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ *main - main entry point
  *
- * Return: Always 0 (Success)
+ *return : always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
